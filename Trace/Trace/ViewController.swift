4d4c8049558b641426701377dd1b9caa0a275670
이희제 <holim1226@gmail.com>
2020-11-10 16:45:33 +0900

    base

---
+++ Trace/Trace/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Trace
+//
+//  Created by 이희제 on 2020/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
